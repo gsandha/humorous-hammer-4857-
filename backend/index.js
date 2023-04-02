f9dc3f55c40bbf71a4a0ae28c4729d89495be87e
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-04-02 15:48:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,4 @@
-const express=require("express")
-const connection=require("./configs/db")
-require("dotenv").config()
-const cors=require("cors")
-const userRouter = require("./Routes/auth.routes")
-const app=express()
-app.use(express.json())
-const port=process.env.port|| 9090
 
-app.use(cors())
-app.use("/user",userRouter)
-app.get("/",(req,res)=>{
-    res.send("Welcome to homepage")
-})
-app.listen(port,async()=>{
-    try {
-        await connection
-        console.log("Connnection succesfull")
-    } catch (error) {
-      console.log(error)  
-    }
-    console.log(`Running at port ${port}`)
-})
 const express = require("express");
 const connection = require("./Configs/db");
 require("dotenv").config();
