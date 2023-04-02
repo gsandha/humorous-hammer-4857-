a54b7eba2f6bc856d948778bbe597c0784c1741c
gsandha <sandhagurpreet0@gmail.com>
2023-04-02 15:09:20 +0530

    Edit

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -5,15 +5,21 @@ import Payments from "../pages/Payments";
 import Login from "../pages/Login";
 import Register from "../pages/Signup";
 import { OtpPage } from "../pages/otp";
+import Home from "../pages/Home";
+import Simple from "../pages/SingleProductPage";
+import Mens from "../pages/Mens";
+
 const AllRoutes=()=>{
     return (
         <Routes>
+            <Route path="/" elements={<Home/>}/>
             <Route path="/register" elements={<Register/>}/>
             <Route path="/login" elements={<Login/>}/>
             <Route path="/cart" elements={<Cart/>}/>
             <Route path="/payments" elements={<Payments/>}/>
             <Route path="/login/otp" elements={<OtpPage/>}/>
-            
+            <Route path="/mens" elements={<Mens/>}/>
+            <Route path="/simple" elements={<Simple/>}/>
         </Routes>
     )
 }
