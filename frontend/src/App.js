d17b36290056e4e2231a221e20f03db12f593c50
gsandha <sandhagurpreet0@gmail.com>
2023-04-02 16:35:37 +0530

    Payments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,12 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Allroutes from "./components/Allroutes";
 import "react-toastify/dist/ReactToastify.css";
-import Cart from "./pages/Cart";
+
 function App() {
   return (
     <div className="App">
-      {/* <Navbar />
-      <Allroutes /> */}
-      <Cart/>
+      <Navbar />
+      <Allroutes />
     </div>
   );
 }
